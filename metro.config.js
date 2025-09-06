f8024ed971da3217e1fac31550efc145c7944b41
suyashpotdar <suyashpotdar03@gmail.com>
2025-09-06 14:11:15 +0530

    implement sanity typegen, build exercise card component, build exercise screen

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,13 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
+
+// 👇 Tell Metro to use the CommonJS build of rxjs instead of esm5
+config.resolver.extraNodeModules = {
+  ...(config.resolver.extraNodeModules || {}),
+  rxjs: path.resolve(__dirname, "node_modules/rxjs/dist/cjs"),
+};
 
 module.exports = withNativeWind(config, { input: "./src/global.css" });
